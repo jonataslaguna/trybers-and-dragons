549b5daa90080b29b367096fceb014fd08a37622
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-11-29 16:02:04 -0300

    implemented heiress class Dwarf, Elf, Halfling and Orc

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints = 60;
+  private static _instances = 0;
+  
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Halfling._instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling._instances;
+  }
+}
---
+++ src/Races/Orc.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private _maxLifePoints = 74;
+  private static _instances = 0;
+  
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Orc._instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc._instances;
+  }
+}
